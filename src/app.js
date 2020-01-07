e15c8893ade7f66c289c6e303f123cff85db2231
William Cofer <willcofer@gmail.com>
2020-01-07 10:46:23 -0600

    backend
    initial commit

---
+++ src/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
+const api = require('./api');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'The world is ending'
+  });
+});
+
+app.use('/api/v1', api);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../../client/build"));
+}
+
+require('backend/src/db');
+
+
+app.listen(PORT, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
+
+module.exports = app;
